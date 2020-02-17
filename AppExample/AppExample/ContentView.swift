92a3985b0d666c85e32b2e1e57d67b20b255402a
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-02-17 17:55:32 -0500

    Initial commit

---
+++ AppExample/AppExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppExample
+//
+//  Created by Lyndsey Ferguson on 2/17/20.
+//  Copyright © 2020 Lyndsey Ferguson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
