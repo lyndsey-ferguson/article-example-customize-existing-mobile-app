c4f07806008941b029cb1c5fc0a91d860bdfe32d
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-04-01 17:56:30 -0400

    WIP: add more untested code to sign app

--- fastlane/customize_built_app.rb
+++ fastlane/customize_built_app.rb
@@ -4,6 +4,7 @@ def customize_built_app(options)
   customer_assets = 'puppy'
 
   customer_appiconset_dirpath = File.absolute_path("../#{customer_assets}/#{customer_assets}.appiconset")
+  customer_profile_pathname = File.absolute_path("../#{customer_assets}/#{customer_assets}.mobileprovision")
 
   zip_package_path = download_latest_release
   # unzip the ipa/images package into a tmp directory
@@ -22,9 +23,16 @@ def customize_built_app(options)
           byebug
           copy_customer_images(customer_appiconset_dirpath, latest_release_pkg_path)
           compile_images(latest_release_pkg_path, example_ipa_payload_dir)
-          # unlock keychain
+
+          # use the 'lyndsey' keychain for now
+          keychain_data = get_keychain_from_vault(vault_addr: 'http://127.0.0.1:8200', keychain_name: 'lyndsey', keychain_path: '/Users/lyndsey.ferguson/Library/Keychains/lyndsey.keychain-db')
+          unlock_keychain(path: keychain_data[:keychain_path], password: keychain_data[:keychain_password], set_default: true)
           # get customer profile, code signing id, and team id
-          # code sign
+          cert = '' # find the cert!
+          sign_frameworks(cert, keychain_data[:keychain_path])
+          bundle_path = 'Payload/AppExample.app'
+          prepare_bundle(bundle_path, customer_profile_pathname)
+	  # code sign
           puts Dir.pwd
         end
       end
@@ -64,4 +72,39 @@ def compile_images(latest_release_pkg_path, example_ipa_payload_dir)
   command += "--output-partial-info-plist #{latest_release_pkg_path}/build/partial.plist "
   byebug
   sh(command)
+end
+
+def sign_frameworks(cert, keychain_filepath)
+  frameworks = Dir.glob('Payload/AppExample.app/Frameworks/*.dylib').map { |s| "'#{s}'" }.join(' ')
+  sh("/usr/bin/codesign -f --keychain \"#{keychain_filepath.shellescape}\"  -s \"#{cert}\" #{frameworks}")
+end
+
+def prepare_bundle(bundle_path, profile_pathname)
+  # Remove existing signature file
+  FileUtils.rm_rf("Payload/#{bundle_path}/_CodeSignature")
+
+  # Copy associated provisioning profile to app
+  FileUtils.cp(profile_pathname, "Payload/#{bundle_path}/embedded.mobileprovision")
+end
+
+def prepare_entitlements(cert, bundle_path, keychain_filepath)
+  app_path = "Payload/#{bundle_path}"
+  # File exists in builds that were created with xcode versions < 10.
+  # Entitlements are now embedded in the app binary which are read and manipulated before signing them back into the binary.
+  FileUtils.rm_f("Payload/#{bundle_path}/#{File.basename(bundle_path, '.*')}.entitlements")
+  entitlements = Tempfile.new()
+  sh("/usr/bin/codesign -d --entitlements :\"#{entitlements.path}\" \"#{app_path}\"")
+  customize_xml_bundle_id(entitlements.path)
+  sh("/usr/bin/codesign -f --keychain \"#{keychain_filepath.shellescape}\" -s \"#{cert}\" --entitlements \"#{entitlements.path}\" \"#{app_path}\"")
+end
+
+def customize_xml_bundle_id(xml_path, customer_app_id, customer_team_id)
+  xml_hash = Plist.parse_xml(xml_path)
+
+  xml_hash['application-identifier'] = customer_app_id
+  xml_hash['com.apple.developer.team-identifier'] = customer_team_id if xml_hash.key?('com.apple.developer.team-identifier')
+
+  entitlement_ids.each { |key, value| xml_hash[key] = value if xml_hash.key?(key) }
+
+  xml_hash.save_plist(xml_path)
 end
