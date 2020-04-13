24f5df780b9c6f7ee79d88e086348d38fb66d2ee
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-04-13 15:56:53 -0400

    add android customization

--- fastlane/README.md
+++ fastlane/README.md
@@ -39,6 +39,15 @@ fastlane ios customize_built_app
 
 ----
 
+## Android
+### android customize_build
+```
+fastlane android customize_build
+```
+
+
+----
+
 This README.md is auto-generated and will be re-generated every time [fastlane](https://fastlane.tools) is run.
 More information about fastlane can be found on [fastlane.tools](https://fastlane.tools).
 The documentation of fastlane can be found on [docs.fastlane.tools](https://docs.fastlane.tools).
---
+++ fastlane/ios_build_custom_app.rb
@@ -0,0 +1,46 @@
+
+def build_custom_app(options)
+  customer_assets = options[:customer_assets] || ENV['APPIAN_CUSTOMER_ASSETS'] || 'lyndsey'
+  customize_build options
+  keychain_data = get_keychain_from_vault(
+    vault_addr: 'http://127.0.0.1:8200',
+    keychain_name: customer_assets
+  )
+  unlock_keychain(
+    path: keychain_data[:keychain_path],
+    password: keychain_data[:keychain_password],
+    set_default: true
+  )
+  disable_automatic_code_signing(path: './iOSExample/iOSExample.xcodeproj')
+  code_signing_identity = code_signing_identity_from_keychain(keychain_data[:keychain_path])
+  update_project_provisioning(
+    xcodeproj: './iOSExample/iOSExample.xcodeproj',
+    profile: "./iOSExample/Yillyyally.mobileprovision",
+    build_configuration: "Release",
+    code_signing_identity: code_signing_identity
+  )
+  update_project_team(
+    path: './iOSExample/iOSExample.xcodeproj',
+    teamid: '57738V598V'
+  )
+  build_app(
+      scheme: 'iOSExample',
+      project: './iOSExample/iOSExample.xcodeproj',
+      output_directory: 'test_output',
+      output_name: 'example.ipa',
+      export_options: {
+        method: "app-store",
+        provisioningProfiles: {
+          "com.yilly.yally" => "359e767c-5f71-4b2e-aedd-17645f951e02"
+        }
+      },
+      export_team_id: '57738V598V',
+      xcargs: "CODE_SIGN_IDENTITY=\"#{code_signing_identity}\""
+  )
+end
+
+def code_signing_identity_from_keychain(keychain_filepath)
+  identity_output = Fastlane::Actions.sh('security', 'find-identity', '-v', '-p', 'codesigning', keychain_filepath)
+  UI.user_error!('Keychain does not contain a single valid signing identity') unless identity_output.match(/1 valid identities found/)
+  identity_output.lines.first.chomp.sub(/.*"([^"]+)".*/, '\1')
+end
