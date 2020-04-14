69328e3d415479acef13e73dda505e139bf3be83
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-04-14 18:08:46 -0400

    get keystore from vault

--- fastlane/android_build_custom_app.rb
+++ fastlane/android_build_custom_app.rb
@@ -5,13 +5,12 @@ def build_custom_app(options)
   unsigned_aligned_apk_path = "../AndroidExample/app/build/outputs/apk/release/app-release-unsigned-aligned.apk"
   signed_apk_path = "../AndroidExample/app/build/outputs/apk/release/app-release.apk"
   FileUtils.rm_rf([unsigned_aligned_apk_path, signed_apk_path])
-  keystore_path = prompt(
-    text: "Keystore: "
+  keystore_data = get_keystore_from_vault(
+    vault_addr: 'http://127.0.0.1:8200',
+    keystore_name: 'lyndsey'
   )
-  keystore_password = prompt(
-    text: "Keystore password: ",
-    secure_text: true
-  )
+  keystore_path = keystore_data[:keystore_path]
+  keystore_password = keystore_data[:keystore_password]
   sh("zipalign -v -p 4 #{unsigned_unaligned_apk_path} #{unsigned_aligned_apk_path}")
   sh("apksigner sign --ks #{keystore_path} --ks-pass pass:'#{keystore_password}' --out #{signed_apk_path} #{unsigned_aligned_apk_path}")
 end
